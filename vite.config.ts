df0c386a516bc6f75e9b422616cb7a088f307fef
Mituha Mizuki <mituha@restar.jp>
2026-05-06 08:34:54 +0900

    最初のコミット

---
+++ vite.config.ts
@@ -0,0 +1,50 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), dts({
+    tsconfigPath: './tsconfig.app.json',
+    include: ['src'],
+    insertTypesEntry: true,
+    bundleTypes: true
+  })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'restar-app',
+      fileName: 'index'
+    },
+    rollupOptions: {
+      external: [
+        'react', 'react-dom',
+        'react/jsx-runtime',
+        'react/jsx-dev-runtime',
+        'react-markdown',
+        'lucide-react'
+      ],
+      output: [
+        {
+          format: 'es',
+          entryFileNames: 'index.js',
+          preserveModules: false
+        },
+        {
+          format: 'umd',
+          name: 'restar-app',
+          entryFileNames: 'index.umd.cjs',
+          globals: {
+            react: 'React',
+            'react-dom': 'ReactDOM',
+            'react/jsx-runtime': 'jsxRuntime',
+            'react/jsx-dev-runtime': 'jsxDevRuntime',
+            'lucide-react': 'Lucide'
+          }
+        }
+      ]
+    },
+    minify: true
+  }
+})
